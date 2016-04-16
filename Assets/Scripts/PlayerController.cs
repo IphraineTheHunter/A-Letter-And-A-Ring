aaf490c09c5611ea0fd6f999eaa11203b0ec341a
dastardlyvandal <dastardlyvandal17@gmail.com>
2016-04-16 16:41:55 -0700

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	/*void Update()
+	{
+		
+	}*/
+
+	//public float speed;
+	//private Vector3 targetPosition;
+
+	public Transform destination;
+	private NavMeshAgent player;
+//	private Rigidbody player;
+
+	void ProcessMovement()
+	{
+		/* float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		if (moveHorizontal == 0 && moveVertical == 0) {
+			player.AddForce (movement);
+		} else {
+			player.AddForce (movement * speed);
+		}*/
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray screenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hit;
+			if (Physics.Raycast(screenRay, out hit))
+			{
+				player.SetDestination(hit.point);
+			}
+		}
+
+		//player.transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+
+	}
+
+	void Start()
+	{
+		player = GetComponent<NavMeshAgent> ();
+	}
+
+
+
+	void FixedUpdate()
+	{
+		ProcessMovement ();
+	}
+}
